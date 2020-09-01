73828529f2f1d11ef3f5e1870f331cbd94236a8f
Vineet Soni <vineetsoni17@gmail.com>
2020-09-02 00:17:32 +0530

    contour hough perspective and neural network missing

--- Database/divider.py
+++ Database/divider.py
@@ -3,11 +3,15 @@ import numpy as np
 import os 
 
 y=0
-for i in os.listdir():
-    img = cv2.imread(i, cv2.IMREAD_GRAYSCALE)
-    height = img.shape[0]
-    width = img.shape[1]
+for i in os.listdir("Database"):
+    if i == "divider.py":
+        continue
+    img = cv2.imread(os.path.join('Database', i), cv2.IMREAD_GRAYSCALE)
+    height = img.shape[0]//9
+    width = img.shape[1]//9
     for j in range(0,9):
         for k in range(0,9):
-            x = img[height*j-1:height*(j+1)-1, width*k-1:width*(k+1):]
+            x = img[height*j+1:height*(j+1)+1, width*k+1:width*(k+1)+1]
+            a = cv2.resize(x, (50, 50), interpolation=cv2.INTER_AREA)
+            cv2.imwrite(os.path.join('Database', f"{str(y)}{str(j)}{str(k)}.jpg"), a)    
     y+=1
--- two.py
+++ two.py
@@ -2,11 +2,13 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-im=cv2.imread("sudoku-puzzle-games.jpg")
+u="WhatsApp Image 2020-08-28 at 1.22.23 PM.jpeg"
+im=cv2.imread(u)
+print(type(im))
 bw=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
 x=cv2.GaussianBlur(bw,(1,1),cv2.BORDER_DEFAULT)
 ret, inv=cv2.threshold(x, 170, 255, cv2.THRESH_BINARY_INV)
-canny=cv2.Canny(inv, 0, 0)
+canny=cv2.Canny(inv, 200, 150)
 lines=cv2.HoughLines(canny, 1, np.pi/180, 300)
 
 grid_marked=im
@@ -23,7 +25,7 @@ for i in range(len(lines)):
         cv2.line(grid_marked, (x1,y1), (x2,y2), (0,255,0), 3)
 
 cv2.imwrite('grid_marked.png', grid_marked)
-plt.imshow(grid_marked)
+plt.imshow(canny, cmap='gray')
 plt.show()
 
 
